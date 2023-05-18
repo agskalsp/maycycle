b9420ad59ed9382832b4a0657b0b70ef7d7fea3a
Lab User <labuser@ip-172-31-2-122.ap-south-1.compute.internal>
2023-05-18 16:02:58 +0000

    update folder structure

---
+++ exam-setup.sh
@@ -0,0 +1,11 @@
+#zip -r question-resources.zip /home/labuser/Desktop/Project/*
+cd /home/labuser/Desktop/Project/
+git clone https://github.com/agskalsp/maycycle.git
+cd /home/labuser/Desktop/Project/maycycle/quessolution/
+cp -r /home/labuser/Desktop/Project/* /home/labuser/Desktop/Project/maycycle/quessolution/
+git status
+git add .
+#git commit -am "Question and Resources Added"
+git commit -am "update setup.sh"
+git push https://ghp_@PRmO@0XJkp@zA5YXj2@miNwc3j@d7WfqOt@1z82@Ok@github.com/agskalsp/maycycle.git
+git status
